af4876ccf49285c004347994ec4d087b91de485c
eands9@yahoo.com <eands9@yahoo.com>
2023-11-12 08:36:02 -0600

    4th

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,47 @@
 <template>
-<div>Test</div>
+  <div>Test</div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            students: []
+  data() {
+    return {
+      students: [],
+    };
+  },
+  mounted() {
+    this.getUpdates();
+  },
+  methods: {
+    async getUpdates() {
+      // const endpoint='/data-api/graphql/Update'
+      // const response = await fetch(endpoint)
+      // const data = await response.json()
+      // console.log(data)
+
+      const query = `
+      {
+          updates {
+          items{
+            email,
+            bdate,
+            zip,
+            status
+          },
+        endCursor,
+        hasNextPage
         }
+      }`;
+
+      const endpoint = "/data-api/graphql";
+      const response = await fetch(endpoint, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query: query }),
+      });
+      const result = await response.json();
+      console.table(result.data.people.items);
     },
-    mounted(){
-        this.getUpdates()
-    },
-    methods:{
-        async getUpdates(){
-          const endpoint='/data-api/graphql/Update'
-          const response = await fetch(endpoint)
-          const data = await response.json()
-          console.log(data)
-        }
-    }
-}
+  },
+};
 </script>
